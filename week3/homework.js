18dc1da286bdae6570c4b90b613327b1fd1e0354
ychen77jojo <nancychen1214@gmail.com>
2022-10-05 16:33:23 -0700

    homework week3

---
+++ week3/homework.js
@@ -0,0 +1,15 @@
+document.getElementById("t1").addEventListener("click",()=>{
+    document.getElementById("display") .src="img/1.png"
+})
+
+document.getElementById("t2").addEventListener("click",()=>{
+    document.getElementById("display") .src="img/2.png"
+})
+
+document.getElementById("t3").addEventListener("click",()=>{
+    document.getElementById("display") .src="img/3.png"
+})
+
+document.getElementById("t4").addEventListener("click",()=>{
+    document.getElementById("display") .src="img/4.png"
+})
